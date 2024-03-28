6cc2912ff853e087090f5f9fb5fb3a9502df5f5f
김수환 <suhwan@kakao.com>
2024-03-29 00:11:17 +0900

    feat: log error 슬랙에 연동 (#15)

--- src/main/java/com/hanghae/theham/HelloController.java
+++ src/main/java/com/hanghae/theham/HelloController.java
@@ -1,14 +1,17 @@
 package com.hanghae.theham;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 public class HelloController {
 
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
+        log.error("한국어 테스트");
         return ResponseEntity.ok().body("pong");
     }
 }
