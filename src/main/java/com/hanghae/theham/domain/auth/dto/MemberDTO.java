53f948310d558e5109cec355b9dae1f4e72bdc2b
SangHoon Kang <53715807+totohoon02@users.noreply.github.com>
2024-04-01 10:24:58 +0900

    feat: 소셜 로그인 기능 (카카오, 네이버, 구글) (#24)
    * feat: 네이버, 카카오 로그인
    
    * feat: 구글로그인 정보가져오기
    
    * feat: OAuthService 통합

---
+++ src/main/java/com/hanghae/theham/domain/auth/dto/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.hanghae.theham.domain.auth.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class MemberDTO {
+    private String id;
+    private String email;
+    private String nickname;
+    private String profileUrl;
+}
