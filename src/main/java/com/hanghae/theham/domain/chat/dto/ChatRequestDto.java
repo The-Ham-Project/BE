153be4a66fcbfb8f7c16d80464ee9c7d6ce51fe1
ramizzang <rkfkaals@gmail.com>
2024-04-02 22:11:27 +0900

    fix : @NotBlank 로 공백 방지

--- src/main/java/com/hanghae/theham/domain/chat/dto/ChatRequestDto.java
+++ src/main/java/com/hanghae/theham/domain/chat/dto/ChatRequestDto.java
@@ -1,16 +1,16 @@
 package com.hanghae.theham.domain.chat.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 public class ChatRequestDto {
 
     @Getter
     public static class ChatRoomCreateRequestDto{
-        @NotNull(message = "채팅을 시작할 상대의 닉네임이 있어야 합니다.")
+        @NotBlank(message = "채팅을 시작할 상대의 닉네임이 있어야 합니다.")
         @Schema(description = "채팅할 상대의 닉네임", example = "카카오라미")
         private String nickname;
     }
-    
+
 }
