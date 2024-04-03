06723c7463f3000312d72338bbea2006e485c4a3
openmpy <suhwan@kakao.com>
2024-04-03 19:49:00 +0900

    fix: List 추가

--- src/main/java/com/hanghae/theham/domain/rental/repository/RentalRepository.java
+++ src/main/java/com/hanghae/theham/domain/rental/repository/RentalRepository.java
@@ -8,11 +8,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface RentalRepository extends JpaRepository<Rental, Long> {
 
     Slice<Rental> findAllByCategoryOrderByCreatedAt(CategoryType categoryType, Pageable pageable);
     Slice<Rental> findSliceBy(Pageable pageable);
-    List<Rental> findAllByCategoryOrderByCreatedAt(CategoryType categoryType);
 
     @Modifying
     @Query(value = "DELETE FROM rental_tbl WHERE id = :id", nativeQuery = true)
