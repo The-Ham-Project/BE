05f2f5b9b0faa8e834866fc34bee6e41542d2ea5
ramizzang <rkfkaals@gmail.com>
2024-04-04 07:55:08 +0900

    refactor : ChatRoomCreat 로직 수정_Rental 추가

--- src/main/java/com/hanghae/theham/domain/chat/service/ChatService.java
+++ src/main/java/com/hanghae/theham/domain/chat/service/ChatService.java
@@ -1,9 +1,11 @@
 package com.hanghae.theham.domain.chat.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
+@Transactional(readOnly = true)
 public class ChatService {
 }
