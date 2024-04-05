264780e4d5ec151877f5d371904faec2047c20ef
김수환 <suhwan@kakao.com>
2024-04-05 10:52:32 +0900

    feat: 랜덤 닉네임 추가 (#47)

--- src/main/java/com/hanghae/theham/domain/member/repository/MemberRepository.java
+++ src/main/java/com/hanghae/theham/domain/member/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.hanghae.theham.domain.member.repository;
 
 import com.hanghae.theham.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -16,4 +17,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByNaverId(String naverId);
 
     Optional<Member> findByNickname(String nickname);
+
+    @Query("SELECT MAX(CAST(SUBSTRING(m.nickname, 4) AS int)) FROM Member m WHERE m.nickname LIKE '더함이%'")
+    Integer findMaxNicknameSequence();
 }
