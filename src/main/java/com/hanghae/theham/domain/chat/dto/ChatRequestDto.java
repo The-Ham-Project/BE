a084ab1d062861006bc2104e1069459c30fa6c07
ramizzang <rkfkaals@gmail.com>
2024-04-05 20:12:12 +0900

    fix : 리뷰 사항 반영

--- src/main/java/com/hanghae/theham/domain/chat/dto/ChatRequestDto.java
+++ src/main/java/com/hanghae/theham/domain/chat/dto/ChatRequestDto.java
@@ -4,6 +4,7 @@ import com.hanghae.theham.domain.chat.entity.Chat;
 import com.hanghae.theham.domain.chat.entity.ChatRoom;
 import com.hanghae.theham.domain.member.entity.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -11,10 +12,10 @@ public class ChatRequestDto {
 
     @Getter
     public static class ChatSendMessageRequestDto {
-        @NotNull(message = "채탱 보내는 멤버의 닉네임을 입력해 주세요")
+        @NotBlank(message = "채탱 보내는 멤버의 닉네임을 입력해 주세요")
         @Schema(description = "로그인 한 멤버 nickname", example = "더함이0001")
         private String nickname;
-        @NotNull(message = "메세지 내용을 입력해주세요.")
+        @NotBlank(message = "메세지 내용을 입력해주세요.")
         @Schema(description = "채팅 메세지 내용", example = "안녕하세요? 함께쓰기 문의합니다")
         private String message;
 
