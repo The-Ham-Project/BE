380b7aee0e3a2f2c7327bb4cc69addac0aba5410
ramizzang <rkfkaals@gmail.com>
2024-04-05 11:17:23 +0900

    rename : chatId 반환 이름 변경

--- src/main/java/com/hanghae/theham/domain/chat/dto/ChatResponseDto.java
+++ src/main/java/com/hanghae/theham/domain/chat/dto/ChatResponseDto.java
@@ -10,14 +10,14 @@ public class ChatResponseDto {
 
     @Getter
     public static class ChatReadResponseDto{ //메세지 반환
-        private Long roomId;
+        private Long chatId;
         private String sender;
         private String message;
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         private LocalDateTime createdAt;
 
         public ChatReadResponseDto(Chat chat) {
-            this.roomId = chat.getId();
+            this.chatId = chat.getId();
             this.message = chat.getMessage();
             this.sender = chat.getSender().getNickname();
             this.createdAt = chat.getCreatedAt();
