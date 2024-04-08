5a07c22cfe170c5be7703dbc628b1c8c6e44614f
openmpy <suhwan@kakao.com>
2024-04-09 03:04:16 +0900

    feat: 회원 정보 조회 기능 추가

--- src/main/java/com/hanghae/theham/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/hanghae/theham/domain/member/dto/MemberResponseDto.java
@@ -6,6 +6,20 @@ import lombok.Getter;
 public class MemberResponseDto {
 
     @Getter
+    public static class MemberReadResponseDto {
+
+        private final String nickname;
+        private final String email;
+        private final String profileUrl;
+
+        public MemberReadResponseDto(Member member) {
+            this.nickname = member.getNickname();
+            this.email = member.getEmail();
+            this.profileUrl = member.getProfileUrl();
+        }
+    }
+
+    @Getter
     public static class MemberUpdatePositionResponseDto {
         private final Long id;
         private final double latitude;
