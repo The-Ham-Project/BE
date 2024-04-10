e2a67061093d88dfc4622d652289dadae4663007
ramizzang <rkfkaals@gmail.com>
2024-04-10 18:52:17 +0900

    feat: 채팅 페이징 처리 추가

--- src/main/java/com/hanghae/theham/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/hanghae/theham/domain/chat/repository/ChatRepository.java
@@ -1,12 +1,12 @@
 package com.hanghae.theham.domain.chat.repository;
 
-import com.hanghae.theham.domain.chat.dto.ChatResponseDto;
 import com.hanghae.theham.domain.chat.entity.Chat;
 import com.hanghae.theham.domain.chat.entity.ChatRoom;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    List<Chat> findByChatRoomOrderByIdDesc(ChatRoom chatRoom);
+    List<Chat> findByChatRoom(ChatRoom chatRoom, PageRequest pageRequest);
 }
