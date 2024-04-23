5a5b24152e4255cdc3b5c055c3ab0f9773e07abc
openmpy <suhwan@kakao.com>
2024-04-23 12:19:20 +0900

    fix: 욕설 필터링 추가 후 배포 환경에서 500 에러 수정

--- src/main/java/com/hanghae/theham/global/util/BadWordFilteringUtil.java
+++ src/main/java/com/hanghae/theham/global/util/BadWordFilteringUtil.java
@@ -4,7 +4,9 @@ import com.vane.badwordfiltering.BadWordFiltering;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class BadWordFilteringUtil {
 
@@ -13,8 +15,18 @@ public class BadWordFilteringUtil {
         ClassPathResource resource = new ClassPathResource("badwords.txt");
 
         try {
-            File file = resource.getFile();
-            badWordFiltering.readFile(file, ",");
+            InputStream inputStream = resource.getInputStream();
+            File tempFile = File.createTempFile("temp-badwords", ".txt");
+            tempFile.deleteOnExit();
+
+            try (FileOutputStream out = new FileOutputStream(tempFile)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+            }
+            badWordFiltering.readFile(tempFile, ",");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
